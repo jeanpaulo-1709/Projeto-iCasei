fd1906298e97ef28730f71ae6fd83e48bdc492eb
jeanpaulo1709 <jeanpaulo17.comercial@gmail.com>
2017-07-05 04:07:08 -0300

    Adicionado paginação a página de pesquisa.

--- WebContent/js/ControllerInicio.js
+++ WebContent/js/ControllerInicio.js
@@ -3,11 +3,17 @@ var app = angular.module('appInicio', []);
 app.controller('ControllerInicio', function($scope, $http) {
 	var key = 'AIzaSyCkhDgkyPJJSYCY6Z7U2jQCA0lbMVEV6E8'
 
+	//Método responsável por fazer a requisição HTTP, passando como parâmetro a String da pesquisa.	
 	$scope.submitPesquisa = function() {
 				
 		 $http.get('https://www.googleapis.com/youtube/v3/search?part=id,snippet&q='+$scope.busca.conteudo+'&key='+key)
 		    .then(function(response) {
-		        
+		    	
+		    	//Limpa Array de vídeos para evitar repetições.
+		    	for(var i=0; i<$scope.videos.length;i++) 
+			    $scope.videos.splice(i)
+			    
+			    //Adiciona os vídeos no array.
 		    	for(var i=0; i<response.data.items.length;i++)
 		    	$scope.videos.push(response.data.items[i]);
 		        $scope.nextPage = response.data.nextPageToken;
@@ -18,9 +24,15 @@ app.controller('ControllerInicio', function($scope, $http) {
 	
 	$scope.submitNextPage = function() {
 		
+		//Método responsável por fazer a requisição HTTP, passando como parâmetro a String da pesquisa e Token de Next Page.	
 		 $http.get('https://www.googleapis.com/youtube/v3/search?part=id,snippet&q='+$scope.busca.conteudo+'&pageToken='+$scope.nextPage+'&key='+key)
 		    .then(function(response) {
-		        
+		    	
+		    	//Limpa Array de vídeos para evitar repetições.
+		    	for(var i=0; i<$scope.videos.length;i++) 
+		    	$scope.videos.splice(i)
+		    	
+		    	//Adiciona os vídeos no array e atualiza variáveis next e preview page
 		    	for(var i=0; i<response.data.items.length;i++)
 		    	$scope.videos.push(response.data.items[i]);
 		    	$scope.nextPage = response.data.nextPageToken;
@@ -32,9 +44,15 @@ app.controller('ControllerInicio', function($scope, $http) {
 	
 	$scope.submitPrevPage = function() {
 		
+		//Método responsável por fazer a requisição HTTP, passando como parâmetro a String da pesquisa e Token de Preview Page.	
 		 $http.get('https://www.googleapis.com/youtube/v3/search?part=id,snippet&q='+$scope.busca.conteudo+'&pageToken='+$scope.prevPage+'&key='+key)
 		    .then(function(response) {
-		        
+
+		    	//Limpa Array de vídeos para evitar repetições.
+		    	for(var i=0; i<$scope.videos.length;i++) 
+			    	$scope.videos.splice(i)
+			    
+			    //Adiciona os vídeos no array e atualiza variáveis next e preview page
 		    	for(var i=0; i<response.data.items.length;i++)
 		    	$scope.videos.push(response.data.items[i]);
 		    	$scope.nextPage = response.data.nextPageToken;
